65a0ef519fe14751def79334e7243b12a85b5beb
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-08-10 20:01:07 -0700

    finished task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(void){
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+/**
+ *main-enter point
+ * 
+ *Descrption: the C programe that prints put function
+ *
+ * Return:alawys 0 
+*/
+int main(void)
+{
+       	puts("\"Programming is like building a multilingual puzzle");
+       	return (0);
 }
