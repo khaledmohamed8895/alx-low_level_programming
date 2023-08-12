3e701db02d57041898728e087da9468a35a3f72c
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-08-12 08:31:28 -0700

    finished task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: print a qoute using write function
+ * Description: print a quote using write function
  *    ssize_t write(int fd, const void *buf, size_t count);
  *
  *Return: 1 (error).
 */
 
 int main(void)
-{	char qou[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
 	return (1);
