5c8de626ff0226b7d7c15e18e2de8e5aa0138b5c
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-08-12 07:43:53 -0700

    finished task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  *
- * Description: this function print
+ * Description: A C program that prints with printf function
  *
  * Return: Always 0 (success)
 */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
