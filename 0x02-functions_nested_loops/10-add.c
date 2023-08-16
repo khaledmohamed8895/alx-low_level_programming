ec404ca8c4f61f5d3292c716a6ec943ef128ba31
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-08-15 22:43:05 -0700

    finished task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ *
+ * @x: first input
+ * @z: second input
+ *
+ * Return: sum of x and z
+*/
+
+int add(int x, int z)
+{
+	int sum = x + z;
+
+	return (sum);
+}
