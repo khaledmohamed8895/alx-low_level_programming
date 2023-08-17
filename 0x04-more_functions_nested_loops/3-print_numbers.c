357767cc7b37606168b670f8c80cd49493ea9579
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-08-17 15:31:30 -0700

    finished task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 - 9
+ *		only using _putchar twice
+ *
+ * Return: 0 (success)
+ */
+
+void print_numbers(void);
+{
+	int number = 0;
+
+	do {
+		_putchar(number + 48);
+		number++;
+	} while (number >= 0 && number <= 9);
+	_putchar('\n');
+}
