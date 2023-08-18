7e5dc3e7ddd65d21c666475c7e88085c7e6f28ce
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-08-18 05:07:23 -0700

    finished task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_triangl - prints a triangle
+ * print_triangle - prints a triangle
  *
  * @size: size of the triangle
  *
  * Return: Always 0 (Success)
 */
 
-void print_triangl(int size)
+void print_triangle(int size)
 {
 	int hght, base;
 
