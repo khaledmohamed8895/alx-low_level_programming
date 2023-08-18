9964e8f3b6f48061151057b7aa6b4667f0654b7e
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-08-18 06:34:51 -0700

    finished task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,8 +17,11 @@ void print_diagonal(int n)
 	{
 		for (postn = 1; postn <= n; postn++)
 		{
-			for (space = 1; space <= postn; space++)
-				_putchar(' ');
+			if (n > 1)
+			{	
+				for (space = 1; space <= postn; space++)
+					_putchar(' ');
+			}
 			_putchar(92); /*is equal to '/' char*/
 			_putchar('\n');
 		}
