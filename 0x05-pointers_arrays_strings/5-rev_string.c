1fb7ea5883c38c764690e3af2c690ad35594c7d8
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-08-21 18:47:06 -0700

    b

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,8 +15,9 @@ void rev_string(char *s)
 
 	/*find string lenght without null char*/
 	for (l = 0; s[l] != '\0'; ++l)
+	{
 		;
-
+	}
 	/*swap the string by looping to half the string*/
 	for (i = 0; i < 1 / 2; i++)
 	{
