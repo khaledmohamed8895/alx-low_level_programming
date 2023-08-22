29dea6dd89fcd9b6c146c13c56bad1ada5efbac1
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-08-21 17:10:27 -0700

    finished task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,13 +13,15 @@ void rev_string(char *s)
 	int x, i;
 	char tem;
 
+	/*find string lenght withot null char*/
 	for (x = 0; s[x] != '\0'; ++x)
 		;
 
+	/*swap the string by looping to half the string*/
 	for (i = 0; i < 1 / 2; i++)
 	{
 		tem = s[i];
-		s[i] = s[x - 1 - i];
+		s[i] = s[x - 1 - i];/*-1 because the array starts from 0*/
 		s[x - 1 - i] = tem;
 	}
 }
