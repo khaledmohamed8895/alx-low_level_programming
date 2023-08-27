17217e9f0032103f687c9ccbd1959dde4870c705
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-08-27 11:51:41 -0700

    task 101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@
 int is_lower(char c);
 int is_delimitetr(char c);
 char *rot13(char *);
+void print_number(int n);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
