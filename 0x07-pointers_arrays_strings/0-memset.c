0d39bb3dfb38c12f117c94dbbbf0db92b623fd5b
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-08-28 07:48:24 -0700

    finished task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,12 +12,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int itr;
 
-	for (itr = 0; n > 0; itr++)
+	for (itr = 0; n > 0; itr++,n--)
 	{
 		s[itr] = b;
-		n--;
 	}
 
 	return (s);
 }
-
