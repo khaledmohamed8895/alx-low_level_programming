259647392e7fc1861d86d047c87228c35d2c374a
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-09-01 15:10:10 -0700

    finished task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-    printf("%s\n", argv[0]);
-    (void)argc;
-    return 0;
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }
