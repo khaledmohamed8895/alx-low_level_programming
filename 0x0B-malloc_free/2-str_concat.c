8690581283344d06eb22ab4e000be6efe8ce18ee
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-09-04 11:15:00 -0700

    finished task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,10 +29,13 @@ char *str_concat(char *s1, char *s2)
 	int size_1, size_2, i;
 	char *c;
 
-	if (s1 == 0 && s2 == 0)
+	if (s1 == NULL)
 	{
-		s1 = '\0';
-		s2 = '\0';
+		s1 = "\0";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "\0";
 	}
 
 	size_1 = _strlen(s1);
@@ -46,9 +49,13 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; i <= size_1 + size_2; i++)
 	{
 		if (i < size_1)
+		{
 			c[i] = s1[i];
+		}
 		else
+		{
 			c[i] = s2[i - size_1];
+		}
 	}
 	c[i] = '\0';
 	return (c);
