364fbcc5327e958d8f108b17f9f84b6832713f77
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-09-26 11:37:35 -0700

    fixed betty of  task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,16 +6,18 @@
  *
  * Return: size  of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
-    size_t num = 0;
+	size_t num = 0;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        num++;
-    }
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num++;
+	}
 
-    return (num);
+	return (num);
 }
+
