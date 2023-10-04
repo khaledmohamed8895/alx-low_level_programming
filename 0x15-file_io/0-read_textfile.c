4c0c44b8672ac100dbbefbf5a8e1d496a2d4e559
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-10-04 15:47:22 +0300

    updating

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,17 +10,23 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t bytes;
-	char buf[READ_BUF_SIZE * 8];
+	ssize_t o, r, w;
+	char *ptr;
 
-	if (!filename || !letters)
+	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	ptr = malloc(sizeof(char) * letters);
+	if (ptr == NULL)
 		return (0);
-	bytes = read(fd, &buf[0], letters);
-	bytes = write(STDOUT_FILENO, &buf[0], bytes);
-	close(fd);
-	return (bytes);
+	o = open(filename, O_RDONLY);
+	r = read(o, ptr, letters);
+	w = write(STDOUT_FILENO, ptr, r);
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(ptr);
+		return (0);
+	}
+	free(ptr);
+	close(o);
+	return (w);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
 
-#define READ_BUF_SIZE 1024
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
