a31f8b74a05db6cefce225bc75d283e398888d72
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2023-11-13 16:35:38 -0800

    finished task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -P /tmp https://github.com/khaledmohamed8895/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+wget -P  https://github.com/khaledmohamed8895/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=libgiga.so
 
