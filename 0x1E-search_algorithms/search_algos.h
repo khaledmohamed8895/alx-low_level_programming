6cf87ee2d83f66fc78394805bf920e99ca5dec3d
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-04-11 21:38:05 +0200

    done

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGOS_H_
+#define SEARCH_ALGOS_H_
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binary_search_recursive(int *array, size_t low, size_t high, int value);
+int jump_search(int *array, size_t size, int value);
+size_t min(int a, int b);
+#endif
